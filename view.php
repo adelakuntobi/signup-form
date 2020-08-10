196d47ded65237abe70bc5b59372a21299e725ff
Adelakun Tobiloba <adelakuntobiloba1@gmail.com>
2020-08-10 20:54:52 +0100

    Adjustments made

--- view.php
+++ view.php
@@ -1,45 +1,48 @@
+<?php
+  session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link href="style.css" rel="stylesheet">
+  <link rel="icon" type="image/png" sizes="32x32" href="favicon.png">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans|Raleway&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Roboto+Mono&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Liu+Jian+Mao+Cao|Lobster&display=swap" rel="stylesheet">
+  <title>Sign Up | Display</title>
 </head>
 <body>
   
 </body>
 </html>
-<?php
-$fname = $_POST['fname'];
-$lname = $_POST['lname'];
-$email = $_POST['email'];
-$dob = $_POST['dob'];
-$dept = $_POST['dept'];
-$gender = $_POST['gender'];
 
-
-echo "<body style='background-color: $color;'></body>"; 
-echo test_input($fname)."<br>";
-
-  function test_input($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-  }
-
-?>
 <body>
 
   <div class="container">
   <?php
-  echo test_input($fname)."<br>";
-  echo test_input($lname)."<br>";
-  echo test_input($email)."<br>";
-  echo test_input($dob)."<br>";
-  echo test_input($dept)."<br>";
-  // echo test_input($gender)."<br>";=
+    $email = $_POST['email'];
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $dob = $_POST['dob'];
+    // echo "<body style='background-color: $color;'></body>"; 
+    echo $email;
+
+  // echo $fname."<br>";
+  // echo $_SESSION['lname']."<br>";
+  // echo $_SESSION['email']."<br>";
+  // echo $_SESSION['dob']."<br>";
+  // echo $_SESSION['dept']."<br>";
+  // echo $_SESSION['gender']."<br>";
+
+  // echo $_SESSION['details'][1]
+  // include ('index.php');
+  // include ()
+  // echo $fname = $_POST['fname']
+
+
   ?>
 </div>
 
